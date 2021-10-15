26ded3a6f516760fc1962332f2b0faa282956e1d
sami-p <sami.peterson14@gmail.com>
2021-10-15 13:19:12 -0600

    Modify friendship model and testing to include friend through user

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-describe Friendship do
-  describe 'validations' do
-    it 
+describe Friendship, type: :model do
+  describe 'Associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:friend) }
   end
 end
