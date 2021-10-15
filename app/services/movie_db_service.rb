940edb37919d8f3ac8072e03eb0b15939b49b260
sami-p <sami.peterson14@gmail.com>
2021-10-15 17:42:13 -0600

    Add search movie action

--- app/services/movie_db_service.rb
+++ app/services/movie_db_service.rb
@@ -2,8 +2,12 @@ class MovieDbService < ApiService
 
   BASE_URL = 'https://api.themoviedb.org'
 
-  def top_movies_by_page(page)
-    get_data("#{BASE_URL}/3/movie/top_rated?api_key=#{ENV["movie_db_api"]}&language=en-US&page=#{page}")
+  def self.top_movies_by_page(page)
+    get_data("#{BASE_URL}/3/movie/top_rated?api_key=#{ENV["movie_db_api"]}&language=en-US&page=#{page}")[:results]
+  end
+
+  def self.search_movie(page, title)
+    get_data("#{BASE_URL}/3/search/movie?language=en-US&page=#{page}&query=#{title}&api_key=#{ENV["movie_db_api"]}")
   end
 
   # def top_40
