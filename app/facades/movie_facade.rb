e21597aad73a2391829403675c26d84df36a3b01
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-17 16:10:44 -0700

    Update movies controller

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,7 +1,14 @@
 class MovieFacade
-  def popular
+  def self.popular
     json = MovieDbService.top_movies_by_page(1) + MovieDbService.top_movies_by_page(2)
-    json[:results].map do |result|
+    json.map do |result|
+      Movie.new(result)
+    end
+  end
+
+  def self.search(title)
+    json = MovieDbService.search_movie(1, title) + MovieDbService.search_movie(2, title)
+    json.map do |result|
       Movie.new(result)
     end
   end
