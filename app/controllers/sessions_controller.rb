a0d25dadacf4188c9d314479aad417f33fecd33d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-17 16:10:54 -0700

    Create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_index_path
+      flash[:success] = "Welcome, #{user.username}!"
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Successfully logged out'
+    redirect_to root_path
+  end
+end
