ac37bdfb7a9d29df508e5889130112455dac2c69
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-17 16:11:01 -0700

    Remove login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,18 +14,6 @@ class UsersController < ApplicationController
     redirect_to dashboard_index_path
   end
 
-  def login
-    user = User.find_by(username: params[:username])
-    if user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to dashboard_index_path
-      flash[:success] = "Welcome, #{user.username}!"
-    else
-      flash[:error] = "Sorry, your credentials are bad."
-      render :login_form
-    end
-  end
-
   private
 
   def user_params
