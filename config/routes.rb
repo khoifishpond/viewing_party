fb366aa44df202d5b4b377c2a3b8840533107ced
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-17 16:11:24 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   # get '/login', to: 'users#login_form'
-  post '/login', to: 'users#login'
-  resources :users, only: [:new, :create, :login]
+  get '/sessions', to: 'sessions#create'
+  resources :users, only: [:new, :create]
   resources :dashboard, only: [:index]
   resources :discover, only: [:index]
   resources :friendships, only: [:new, :create]
   resources :movies, only: [:index, :show]
+  resources :sessions, only: [:create, :destroy]
 end
