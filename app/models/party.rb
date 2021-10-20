25a025c5e4ef62abf972b9882962370f489528c7
sami-p <sami.peterson14@gmail.com>
2021-10-19 22:48:07 -0600

    Modify user model relationships

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,7 +1,10 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: 'User'
+
   has_many :party_guests
   has_many :users, through: :party_guests
+
   validates :start_time, presence: true
+  validates :date, presence: true
   validates :duration, numericality: { only_integer: true }, presence: true
 end
