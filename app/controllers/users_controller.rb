b25f5889a4bf208f78e7e3fe81e405d9e95949ca
sami-p <sami.peterson14@gmail.com>
2021-10-19 22:49:03 -0600

    Add invite checkbox to new viewing party page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  # def login_form
-  # end
-
   def new
     @user = User.new
   end
@@ -10,6 +7,7 @@ class UsersController < ApplicationController
     user = user_params
     user[:username] = user[:username].downcase
     new_user = User.create(user)
+    session[:user_id] = new_user.id
     flash[:success] = "Welcome, #{new_user.username}!"
     redirect_to dashboard_index_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, dependent: :destroy
-  has_many :parties, dependent: :destroy
-  has_many :party_guests, through: :parties
+  has_many :party_guests, dependent: :destroy
+  has_many :parties, through: :party_guests
 
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates :email, uniqueness: true, presence: true
 
   has_secure_password
+
+  def self.find_user(id)
+  end
 end
