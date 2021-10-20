6cfc80c3783f905f305b9d0992357e16b0edb49a
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-19 18:22:09 -0700

    Update parties

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,7 @@
 class Party < ApplicationRecord
   belongs_to :host, class_name: 'User'
-
   has_many :party_guests
   has_many :users, through: :party_guests
+  validates :start_time, presence: true
+  validates :duration, numericality: { only_integer: true }, presence: true
 end
