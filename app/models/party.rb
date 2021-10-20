6f36ead939ba3ff5a64b0322f42e110d7ebf2093
Khoi Nguyen <khoinguyen311@gmail.com>
2021-10-20 15:09:24 -0700

    Add find host method

--- app/models/party.rb
+++ app/models/party.rb
@@ -7,4 +7,9 @@ class Party < ApplicationRecord
   validates :start_time, presence: true
   validates :date, presence: true
   validates :duration, numericality: { only_integer: true }, presence: true
+  validates :movie_id, presence: true
+
+  def find_host(user_id)
+    User.find(user_id)
+  end
 end
