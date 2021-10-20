cfa87612eb28cfbc7a514b1e113be0388db55b1b
sami-p <sami.peterson14@gmail.com>
2021-10-19 22:47:04 -0600

    Add conditional to parties controller create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,17 @@
 class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:search])
-    friendship = Friendship.create(user_id: current_user.id, friend_id: friend.id)
+    friendship = Friendship.find_by(user: current_user, friend: friend)
+    if friend && friend.email == current_user.email
+      flash[:error] = "Please enter a valid email address."
+    elsif friend && friendship.nil?
+      current_user.friendships.create(friend_id: friend.id)
+      flash[:success] = "Friendship successfully created."
+    elsif friend && friendship
+      flash[:error] = "You cannot add that friend twice. Please try again."
+    else
+      flash[:error] = "That user does not exist. Please try again."
+    end
     redirect_to '/dashboard'
   end
 
